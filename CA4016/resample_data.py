ca6e2b38cf8bfc20d42ba35b7016d8360d455bf9
lynchp35 <78041488+lynchp35@users.noreply.github.com>
2022-11-19 14:31:08 +0000

    resampled raw sensor data

---
+++ CA4016/resample_data.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import datetime
+import os
+
+def create_resampled_data(path, og_path = "data/D1NAMO/"):
+    colNames = ["V_Mean","L_Mean","S_Mean","V_STD","L_STD","S_STD",
+    "B_WF_Mean","B_WF_STD","ECG_WF_Mean","ECG_WF_STD"]
+
+    for subject_type in os.listdir(path):
+        for subject_ID in os.listdir(f"{path}/{subject_type}"):
+            subject_df = pd.DataFrame(columns=colNames)
+            for day in os.listdir(f"{path}/{subject_type}/{subject_ID}/sensor_data"):
+                try:
+                    df = pd.read_csv(f"{og_path}/{subject_type}/{subject_ID}/sensor_data/{day}/{day}_Summary.csv")
+                    
+                    accel = pd.read_csv(f"{og_path}/{subject_type}/{subject_ID}/sensor_data/{day}/{day}_Accel.csv")
+                    breathing = pd.read_csv(f"{og_path}/{subject_type}/{subject_ID}/sensor_data/{day}/{day}_Breathing.csv")  
+                    ecg = pd.read_csv(f"{og_path}/{subject_type}/{subject_ID}/sensor_data/{day}/{day}_ECG.csv")
+                    current_df = join_files(accel,breathing, ecg)
+                    subject_df.append(current_df)
+                except FileNotFoundError:
+                    print(f"{og_path}/{subject_type}/{subject_ID}/sensor_data/{day}/")
+        subject_df.to_csv(f"{path}/{subject_type}/{subject_ID}/sensor_data/resampled_sensor_data.csv")
+        
+
+def join_files(accel,breathing, ecg):
+    
+    accel["Time"] = pd.to_datetime(accel["Time"], format="%d/%m/%Y %H:%M:%S.%f")
+    breathing["Time"] = pd.to_datetime(breathing["Time"], format="%d/%m/%Y %H:%M:%S.%f")
+    ecg["Time"] = pd.to_datetime(ecg["Time"], format="%d/%m/%Y %H:%M:%S.%f")
+    
+    new_df = accel.set_index("Time").resample("s").mean().copy().rename(
+        columns={"Vertical":"V_Mean","Lateral":"L_Mean","Sagittal":"S_Mean"})
+    
+    accel_std = accel.set_index("Time").resample("s").std().copy().rename(
+        columns={"Vertical":"V_STD","Lateral":"L_STD","Sagittal":"S_STD"})
+    new_df = new_df.join(accel_std)
+    
+    breathing_mean = breathing.set_index("Time").resample("s").mean().copy().rename(
+        columns={"BreathingWaveform":"B_WF_Mean"})
+    new_df = new_df.join(breathing_mean)
+    
+    breathing_std = breathing.set_index("Time").resample("s").std().copy().rename(
+        columns={"BreathingWaveform":"B_WF_STD"})
+    new_df = new_df.join(breathing_std)
+    
+    ecg_mean = ecg.set_index("Time").resample("s").mean().copy().rename(
+        columns={"EcgWaveform":"ECG_WF_Mean"})
+    new_df = new_df.join(ecg_mean)
+    
+    ecg_std = ecg.set_index("Time").resample("s").std().copy().rename(
+        columns={"EcgWaveform":"ECG_WF_STD"})
+    new_df = new_df.join(ecg_std)
+
+    
+    return new_df
+
+if main == "__name__":
+    create_resampled_data(path = "data/processed_data", og_path = "data/D1NAMO")
